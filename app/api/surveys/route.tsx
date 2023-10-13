143d2fc1f159a6aec9c1ab505b8d646a690f2785
Hugo <arceoh@gmail.com>
2023-10-13 15:01:35 -0700

    Clean up comments/imports

--- app/api/surveys/route.tsx
+++ app/api/surveys/route.tsx
@@ -3,7 +3,7 @@ import { dbConnect } from "@/app/db/dbConnect";
 import { Survey } from "@/app/_modles/surveyModel";
 
 export async function GET(request: NextRequest) {
-  // console.log("REQUEST", request);
+  console.log("REQUEST", request);
   console.log("REQUEST URL", request.url);
   const url = request.url;
   // const filters =  params.get("name"); // is the string "Jonathan Smith".
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,9 @@
-import Link from "next/link";
+import camelCaseString from "@/utils/camelCaseString";
 import ColumnToggleMenu from "./Components/ColumnToggleMenu";
-import FilterSwitchButton from "./Components/FilterSwitchButton";
-import FiltersResetButton from "./Components/FiltersResetButton";
+import FilterResetQueryParamsButton from "./Components/FilterResetQueryParamsButton";
 import SearchInput from "./Components/SearchInput";
-import StatusResetButton from "./Components/StatusResetButton";
-import StatusSwitchButton from "./Components/StatusSwitchButton";
 import TableOfSurveys from "./Components/TableOfSurveys";
 import ToggleQueryFParamButton from "./Components/ToggleQueryFParamButton";
-import FilterResetQueryParamsButton from "./Components/FilterResetQueryParamsButton";
-import camelCaseString from "@/utils/camelCaseString";
 
 const filtersList = ["Cash Aid", "IEP", "DLI", "Returning"];
 const statusList = ["New", "Pending", "Enrolled", "Denied"];
@@ -18,8 +13,6 @@ const DashboardHome = ({
 }: {
   searchParams: { [key: string]: string | string[] | undefined };
 }) => {
-  const filters = { ...searchParams };
-
   return (
     <>
       <div className="mx-auto max-w-7xl sm:px-6 lg:px-8 pt-4">
@@ -40,33 +33,6 @@ const DashboardHome = ({
                   />
                 );
               })}
-              {/* <ToggleQueryFParamButton
-                filterSlug="cashAid"
-                title="Cash Aid"
-                searchParams={searchParams}
-              />
-              <br />
-              <ToggleQueryFParamButton
-                filterSlug="iep"
-                title="IEP"
-                searchParams={searchParams}
-              />
-              <ToggleQueryFParamButton
-                filterSlug="dli"
-                title="DLI"
-                searchParams={searchParams}
-              />
-              <ToggleQueryFParamButton
-                filterSlug="returning"
-                title="Returning"
-                searchParams={searchParams}
-              /> */}
-              {/*
-              <FiltersResetButton />
-              <FilterSwitchButton title="Cash Aid" filterSlug="cashAid" />
-              <FilterSwitchButton title="IEP" filterSlug="IEP" />
-              <FilterSwitchButton title="DLI" filterSlug="DLI" />
-              <FilterSwitchButton title="Returning" filterSlug="Returning" /> */}
             </div>
             <div className="btn-group btn-group-vertical lg:btn-group-horizontal">
               <FilterResetQueryParamsButton
@@ -82,32 +48,6 @@ const DashboardHome = ({
                   />
                 );
               })}
-              {/* <ToggleQueryFParamButton
-                filterSlug="new"
-                title="New"
-                searchParams={searchParams}
-              />
-              <ToggleQueryFParamButton
-                filterSlug="pending"
-                title="Pending"
-                searchParams={searchParams}
-              />
-              <ToggleQueryFParamButton
-                filterSlug="enrolled"
-                title="Enrolled"
-                searchParams={searchParams}
-              />
-              <ToggleQueryFParamButton
-                filterSlug="denied"
-                title="Denied"
-                searchParams={searchParams}
-              /> */}
-              {/*
-              <StatusResetButton />
-              <StatusSwitchButton title="New" StatusSlug="New" />
-              <StatusSwitchButton title="Pending" StatusSlug="Pending" />
-              <StatusSwitchButton title="Enrolled" StatusSlug="Enrolled" />
-              <StatusSwitchButton title="Denied" StatusSlug="Denied" /> */}
             </div>
           </div>
           <div>
