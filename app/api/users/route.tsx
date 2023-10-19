8d03f8a0236f61ee74300e4e5b3100010013a4f9
Hugo <arceoh@gmail.com>
2023-10-19 10:45:27 -0700

    Exposed Api for production testing

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -6,14 +6,14 @@ import { authOptions } from "../auth/[...nextauth]/route";
 import { BASE_URL } from "@/app/config";
 
 export async function GET(request: NextRequest) {
-  const session = await getServerSession(authOptions);
+  // const session = await getServerSession(authOptions);
 
-  if (!session) {
-    return NextResponse.redirect(BASE_URL);
-  }
+  // if (!session) {
+  //   return NextResponse.redirect(BASE_URL);
+  // }
   await dbConnect();
 
   const users = await User.find().limit(10);
 
-  return NextResponse.json({ message: "Hello World", users });
+  return NextResponse.json({ users });
 }
