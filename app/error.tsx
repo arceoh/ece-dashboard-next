de4de0e42fccfa8cc8db04180bc32f5185b1ab28
Hugo <arceoh@gmail.com>
2023-10-20 12:48:39 -0700

    Homepage to ssr

--- app/error.tsx
+++ app/error.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import "./globals.css";
+import Container from "./components/Container";
 
 interface Props {
   error: Error;
@@ -10,19 +11,19 @@ interface Props {
 const error = ({ error, reset }: Props) => {
   console.error(error);
   return (
-    <div>
+    <Container>
       <p>There was an problem</p>
       <h1>Error</h1>
       <p> Please try again or contact support if the problem persists.</p>
       <div className="flex">
-        <button className="btn btn-secondary btn-lg" onClick={reset}>
+        <button className="btn btn-secondary" onClick={reset}>
           Try Again
         </button>
         <Link href="/">
-          <a className="btn btn-primary btn-lg">Go Home</a>
+          <a className="btn btn-primary">Go Home</a>
         </Link>
       </div>
-    </div>
+    </Container>
   );
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,7 @@
-"use client";
-import { useEffect } from "react";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
 import ThemeToggle from "./components/ThemeToggle";
 import SigninWithGoogleButton from "./components/SigninWithGoogleButton";
 
 function HomePage() {
-  const { data: session } = useSession();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (session) {
-      router.push("/dashboard"); // Replace with the desired destination
-    }
-  }, [session, router]);
-
   return (
     <div className="bg-[url('/images/data_bkg.svg')]  bg-no-repeat bg-right-bottom bg-cover sm:bg-contain h-screen">
       <div className="w-1/3 h-full overflow-y-scroll backdrop-blur-sm   md:border-r border-opacity-40   p-8 flex flex-col bg-base-300">
