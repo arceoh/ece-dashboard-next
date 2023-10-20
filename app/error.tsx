83a471e9ce5d1ef43ee06ef30f15c2d71d94787b
Hugo <arceoh@gmail.com>
2023-10-20 12:45:20 -0700

    Style error page

--- app/error.tsx
+++ app/error.tsx
@@ -1,5 +1,6 @@
 "use client";
-import React from "react";
+import Link from "next/link";
+import "./globals.css";
 
 interface Props {
   error: Error;
@@ -10,8 +11,17 @@ const error = ({ error, reset }: Props) => {
   console.error(error);
   return (
     <div>
+      <p>There was an problem</p>
       <h1>Error</h1>
-      <button onClick={reset}>Reset</button>
+      <p> Please try again or contact support if the problem persists.</p>
+      <div className="flex">
+        <button className="btn btn-secondary btn-lg" onClick={reset}>
+          Try Again
+        </button>
+        <Link href="/">
+          <a className="btn btn-primary btn-lg">Go Home</a>
+        </Link>
+      </div>
     </div>
   );
 };
