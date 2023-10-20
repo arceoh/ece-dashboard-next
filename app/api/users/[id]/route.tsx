9c7feb12510246b603f0d860e44d212c366e28a2
Hugo <arceoh@gmail.com>
2023-10-20 14:36:31 -0700

    Update api route

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -4,7 +4,6 @@ import { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../../auth/[...nextauth]/route";
 import { BASE_URL } from "@/app/config";
-import { NextApiRequest } from "next";
 import { headers } from "next/headers";
 
 interface Props {
@@ -13,11 +12,10 @@ interface Props {
 
 // export async function GET(request: NextRequest, { params: { id } }: Props) {
 // export async function GET(request: Request, { params: { id } }: Props) {
-export async function GET(request: NextApiRequest, { params: { id } }: Props) {
+export async function GET(request: Request, { params: { id } }: Props) {
   const session = await getServerSession(authOptions);
 
   const headersList = headers();
-  const referer = headersList.get("referer");
 
   if (!session) {
     return NextResponse.redirect(BASE_URL);
@@ -26,7 +24,7 @@ export async function GET(request: NextApiRequest, { params: { id } }: Props) {
   const user = await User.findById(id);
   // return NextResponse.json(user);
 
-  return new Response(JSON.stringify(user), {
+  return new NextResponse(JSON.stringify(user), {
     status: 200,
     headers: {
       "Content-Type": "application/json",
