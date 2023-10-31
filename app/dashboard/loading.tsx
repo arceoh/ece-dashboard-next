f3ddc913276699bfedb177f48c3ef737a6d627ac
Hugo Arceo <arceoh@gmail.com>
2023-10-31 15:16:22 -0700

    Loading skeleton styles

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -4,6 +4,7 @@ import TableRowSkeleton from "./Components/TableRowSkeleton";
 import { MdFilterAlt, MdOutlineClear } from "react-icons/md";
 import { AiFillCaretDown } from "react-icons/ai";
 import camelCaseString from "@/utils/camelCaseString";
+import Container from "../components/Container";
 
 const DashboardLoadingPage = () => {
   const count = 5;
@@ -14,43 +15,41 @@ const DashboardLoadingPage = () => {
 
   return (
     <>
-      <div className="mx-auto max-w-7xl sm:px-6 lg:px-8 pt-4">
+      <Container>
         <h1>Dashboard</h1>
-        <div className="filters flex justify-between items-center">
-          <div className="space-x-4 flex">
-            <div className="btn-group btn-group-vertical lg:btn-group-horizontal animate-pulse">
-              <div className="btn btn-xs">
-                <MdFilterAlt /> Filter
-              </div>
-              {filtersList.map((filter) => {
-                return (
-                  <div key={camelCaseString(filter)} className={`btn btn-xs`}>
-                    {filter}
-                  </div>
-                );
-              })}
+        <div className="flex justify-start flex-wrap items-center sm:justify-between space-y-2 xl:space-y-0">
+          <div className="btn-group btn-group-horizontal mr-4 animate-pulse">
+            <div className="btn btn-xs">
+              <MdFilterAlt /> Filter
             </div>
-            <div className="btn-group btn-group-vertical lg:btn-group-horizontal animate-pulse">
-              <div className="btn btn-xs">
-                <MdFilterAlt /> Filter
-              </div>
-              {statusList.map((status) => {
-                return (
-                  <div key={camelCaseString(status)} className={`btn btn-xs`}>
-                    {status}
-                  </div>
-                );
-              })}
-            </div>
+            {filtersList.map((filter) => {
+              return (
+                <div key={camelCaseString(filter)} className={`btn btn-xs`}>
+                  {filter}
+                </div>
+              );
+            })}
           </div>
-          <div className="btn btn-xs animate-pulse">
+          <div className="btn-group btn-group-horizontal animate-pulse">
+            <div className="btn btn-xs">
+              <MdFilterAlt /> Filter
+            </div>
+            {statusList.map((status) => {
+              return (
+                <div key={camelCaseString(status)} className={`btn btn-xs`}>
+                  {status}
+                </div>
+              );
+            })}
+          </div>
+          <div className="btn btn-xs animate-pulse mr-2">
             Columns <AiFillCaretDown />
           </div>
           <div>
             <div className="w-44 h-5 bg-base-200 rounded-md"></div>
           </div>
         </div>
-      </div>
+      </Container>
       <div className="overflow-x-auto my-4">
         <table className="table table-zebra">
           <TableHeader />
