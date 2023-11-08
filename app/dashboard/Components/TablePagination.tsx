b479e9d45529f5b759ab9a9f1777be6e563f6dcb
Hugo <arceoh@gmail.com>
2023-11-08 15:26:35 -0800

    Pagination logic and style

--- app/dashboard/Components/TablePagination.tsx
+++ app/dashboard/Components/TablePagination.tsx
@@ -1,4 +1,12 @@
+import { cn } from "@/utils/cn";
 import Link from "next/link";
+import { useMemo } from "react";
+import {
+  TbChevronLeft,
+  TbChevronRight,
+  TbChevronsLeft,
+  TbChevronsRight,
+} from "react-icons/tb";
 
 type Props = {
   searchParams?: { [key: string]: string | string[] | undefined };
@@ -7,28 +15,97 @@ type Props = {
 };
 
 const TablePagination = ({ searchParams, pageCount, currentPage }: Props) => {
-  const pages = Array.from(Array(pageCount).keys());
   const filters = { ...searchParams };
 
+  const displayPageRange = 5; // Maximum number of page links to display
+
+  const pages = useMemo(() => {
+    const pagesToShow = Math.min(displayPageRange, pageCount);
+    const startPage = Math.min(
+      Math.max(1, currentPage - Math.floor(pagesToShow / 2)),
+      pageCount - pagesToShow + 1
+    );
+
+    return Array.from({ length: pagesToShow }, (_, i) => startPage + i);
+  }, [currentPage, pageCount, displayPageRange]);
+
   return (
-    <div className="text-center">
-      <div className="join">
-        {pages.map((page) => {
-          return (
-            <Link
-              key={page + 1}
-              className={`${
-                currentPage === page + 1 ? "btn-active btn-disabled" : ""
-              } join-item btn`}
-              href={{
-                pathname: "/dashboard",
-                query: { ...filters, page: page + 1 },
-              }}
-            >
-              {page + 1}
-            </Link>
-          );
-        })}
+    <div className="text-center pagination">
+      <div className="join bg-base-200">
+        <Link
+          className={cn("join-item btn px-2", {
+            "btn-active cursor-default": currentPage === 1,
+          })}
+          href={{ pathname: "/dashboard", query: { ...filters, page: 1 } }}
+        >
+          <span className="tooltip" data-tip="First Page">
+            <TbChevronsLeft className="text-xl" />
+            <span className="sr-only">First Page</span>
+          </span>
+        </Link>
+
+        <Link
+          className={cn("join-item btn px-2", {
+            "btn-active cursor-default": currentPage - 1 <= 0,
+          })}
+          href={{
+            pathname: "/dashboard",
+            query: { ...filters, page: currentPage - 1 },
+          }}
+        >
+          <span className="tooltip" data-tip="Previous">
+            <TbChevronLeft className="text-lg" />
+            <span className="sr-only">Previous</span>
+          </span>
+        </Link>
+
+        {pages.map((page) => (
+          <Link
+            key={page}
+            className={cn("join-item btn hidden md:flex", {
+              "btn-active cursor-default": currentPage === page,
+            })}
+            href={{
+              pathname: "/dashboard",
+              query: { ...filters, page },
+            }}
+          >
+            {page}
+          </Link>
+        ))}
+        <div className="join-item self-center px-4 bg-base-200 text-sm font-semibold flex md:hidden">
+          Page {currentPage}
+        </div>
+
+        <Link
+          className={cn("join-item btn px-2", {
+            "btn-active cursor-default": currentPage === pageCount,
+          })}
+          href={{
+            pathname: "/dashboard",
+            query: { ...filters, page: currentPage + 1 },
+          }}
+        >
+          <span className="tooltip" data-tip="Next">
+            <TbChevronRight className="text-lg" />
+            <span className="sr-only">Next</span>
+          </span>
+        </Link>
+
+        <Link
+          className={cn("join-item btn px-2", {
+            "btn-active cursor-default": currentPage === pageCount,
+          })}
+          href={{
+            pathname: "/dashboard",
+            query: { ...filters, page: pageCount },
+          }}
+        >
+          <span className="tooltip" data-tip="Last page">
+            <TbChevronsRight className="text-xl" />
+            <span className="sr-only">Last Page</span>
+          </span>
+        </Link>
       </div>
     </div>
   );
