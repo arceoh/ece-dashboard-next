15744adf75350f496f947bf137fabebcf92bab49
Hugo <arceoh@gmail.com>
2023-11-08 11:24:02 -0800

    Clear search text button

--- app/dashboard/Components/SearchInput.tsx
+++ app/dashboard/Components/SearchInput.tsx
@@ -1,57 +1,61 @@
 "use client";
+import { cn } from "@/utils/cn";
 import Link from "next/link";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
-import { MdOutlineClear } from "react-icons/md";
+import { MdFilterAltOff } from "react-icons/md";
 
 const SearchInput = () => {
   const searchParams = useSearchParams();
   const search = searchParams.get("search") || "";
   const router = useRouter();
 
-  const [data, setData] = useState<string>(search || "");
+  const [searchText, setSearchText] = useState<string>(search);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (data.length < 3) {
+    if (searchText.length < 3) {
       return;
     }
-    router.push(`/dashboard?search=${data}`);
+    router.push(`/dashboard?search=${searchText}`);
+  };
+  const clearSearch = () => {
+    setSearchText("");
+    router.push("/dashboard");
   };
 
   useEffect(() => {
-    setData(search);
+    setSearchText(search);
   }, [search]);
 
   return (
     <form className="join" onSubmit={handleSubmit}>
       <input
-        onChange={(e) => setData(e.currentTarget.value)}
-        value={data}
+        onChange={(e) => setSearchText(e.currentTarget.value)}
+        value={searchText}
         type="text"
         placeholder="Search by Name"
         className="input input-bordered join-item input-sm w-full"
         minLength={3}
       />
       <Link
-        href={`/dashboard?search=${data}`}
+        href={`/dashboard?search=${searchText}`}
         prefetch={false}
         type="submit"
         className={`${
-          data.length < 3 ? "btn-disabled" : ""
+          searchText.length < 3 ? "btn-disabled" : ""
         }  btn  btn-sm join-item`}
       >
         Search
       </Link>
-      <Link
-        href={"/dashboard"}
-        replace
-        className={`${
-          data.length < 1 ? "btn-disabled" : ""
-        }  btn  btn-sm join-item`}
+      <button
+        onClick={clearSearch}
+        className={cn("btn  btn-sm join-item", {
+          "btn-disabled": search === "" && searchText.length < 3,
+        })}
       >
-        <MdOutlineClear /> <span className="sr-only">Clear</span>
-      </Link>
+        <MdFilterAltOff /> <span className="sr-only">Clear Search</span>
+      </button>
     </form>
   );
 };
